7b00e4617302e19d99d8929da7b8e67cec18943e
yotadori <yotakunhappy@gmail.com>
2024-03-19 07:51:54 +0900

    mainでudpのクラスを使うように

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,10 @@
 #include <Arduino.h>
 #include "EspEasyTimer.h"
-
-
-
-//////////////////////////////////////////////
-//        RemoteXY include library          //
-//////////////////////////////////////////////
-
-// you can enable debug logging to Serial at 115200
-//#define REMOTEXY__DEBUGLOG    
-
-// RemoteXY select connection mode and include library 
-#define REMOTEXY_MODE__ESP32CORE_BLE
-#include <BLEDevice.h>
-
-#include <RemoteXY.h>
-
-// RemoteXY connection settings 
-#define REMOTEXY_BLUETOOTH_NAME "RemoteXY"
-
-
-
-// RemoteXY configurate  
-#pragma pack(push, 1)
-uint8_t RemoteXY_CONF[] =   // 46 bytes
-  { 255,5,0,0,0,39,0,17,0,0,0,31,1,106,200,1,1,3,0,5,
-  24,17,60,60,32,2,26,31,5,23,106,60,60,32,2,26,31,1,8,166,
-  24,24,0,2,31,0 };
-  
-// this structure defines all the variables and events of your control interface 
-struct {
-
-    // input variables
-  int8_t joystick_01_x; // from -100 to 100}
-  int8_t joystick_01_y; // from -100 to 100}
-  int8_t joystick_02_x; // from -100 to 100}
-  int8_t joystick_02_y; // from -100 to 100}
-  uint8_t button_01; // =1 if button pressed, else =0
-
-    // other variable
-  uint8_t connect_flag;  // =1 if wire connected, else =0
-
-} RemoteXY;
-#pragma pack(pop)
- 
-/////////////////////////////////////////////
-//           END RemoteXY include          //
-/////////////////////////////////////////////
-
 #include "Servo.h"
 #include "Rot_Servo.h"
 #include "Gyro.h"
 #include "Robo.h"
+#include "Udp_Receiver.h"
 
 const int SERVO_PIN = 2;
 const int ROT_PIN_1 = 13;
@@ -67,6 +20,11 @@ Gyro gyro_sens = Gyro();
 
 Robo robo = Robo();
 
+char* ssid = "F660A-xRb9-G";
+char* password = "x9eyusp7";
+
+Udp_Receiver receiver = Udp_Receiver(ssid, password);
+
 void timer1Task() {
   gyro_sens.update();
 }
@@ -74,8 +32,6 @@ void timer1Task() {
 EspEasyTimer timer1(TIMER_GROUP_0, TIMER_0);
 
 void setup() {
-
-  RemoteXY_Init (); 
 
   // put your setup code here, to run once:
   gyro_sens.setup();
@@ -87,6 +43,8 @@ void setup() {
 
   Serial.begin(115200);
 
+  receiver.setup();
+
   // 加速度センサ用割り込み
   // interval 10ms
   timer1.begin(timer1Task, 10);
@@ -97,15 +55,10 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  RemoteXY_Handler ();
-
   // 推定速度
   xyz_t vel {-1 * gyro_sens.vel().z, -1 * gyro_sens.vel().y, -1 * gyro_sens.gyro().x};
   // 目標速度
   xyz_t target_vel{0, 0, 0};
-  target_vel.x = RemoteXY.joystick_01_x;
-  target_vel.y = RemoteXY.joystick_01_y;
-  target_vel.z = RemoteXY.joystick_02_y / 100.0;
 
   robo.update(vel);
   xyz_t out_vel = robo.execute(target_vel);
@@ -118,23 +71,9 @@ void loop() {
   servo3.set_speed((-0.866 * out_vel.x +0.500 * out_vel.y + Robo::RADIUS * out_vel.z) / MAX_SPEED);
 
   // キック
-  if (RemoteXY.button_01 == 1) {
+  if (false) {
     servo0.set_angle(-60);
     delay(100);
     servo0.set_angle(-90);
   }
-
-  Serial.printf(">x_tar:%f\n", target_vel.x);
-  Serial.printf(">y_tar:%f\n", target_vel.y);
-  Serial.printf(">z_tar:%f\n", target_vel.z);
-
-  Serial.printf(">x:%f\n", vel.x);
-  Serial.printf(">y:%f\n", vel.y);
-  Serial.printf(">z:%f\n", vel.z);
-
-  Serial.printf(">x_out:%f\n", out_vel.x);
-  Serial.printf(">y_out:%f\n", out_vel.y);
-  Serial.printf(">z_out:%f\n", out_vel.z);
-
-  RemoteXY_delay(20);
 }
