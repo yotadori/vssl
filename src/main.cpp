87af5f8c4662f8648d3c756da825d4f12790036c
yotadori <yotakunhappy@gmail.com>
2024-03-29 15:02:54 +0900

    dabbleを使えるように

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,19 @@
 #include "Servo.h"
 #include "Rot_Servo.h"
 #include "Robo.h"
-#include "Udp_Receiver.h"
 #include "Speaker.h"
+
+#define USE_DABBLE // Dabbleを使わないならここをコメントアウト
+
+#ifdef USE_DABBLE
+#define CUSTOM_SETTINGS
+#define INCLUDE_GAMEPAD_MODULE
+#include "DabbleESP32.h"
+#endif
+
+#ifndef USE_DABBLE
+#include "Udp_Receiver.h"
+#endif
 
 const int SERVO_PIN = D7;
 const int ROT_PIN_1 = D2;
@@ -15,26 +26,50 @@ const int SPEAKER_PIN = D6;
 Servo servo0 = Servo(0, SERVO_PIN);
 Rot_Servo servo1 = Rot_Servo(1, ROT_PIN_1, 0);
 Rot_Servo servo2 = Rot_Servo(2, ROT_PIN_2, 0);
-Rot_Servo servo3 = Rot_Servo(3, ROT_PIN_3, 20);
+Rot_Servo servo3 = Rot_Servo(3, ROT_PIN_3, 0);
 
 Robo robo = Robo();
 
+#ifndef USE_DABBLE
 char* ssid = "F660A-xRb9-G";
 char* password = "x9eyusp7";
 
 Udp_Receiver receiver = Udp_Receiver(ssid, password);
+#endif
 
 Speaker speaker = Speaker(4, SPEAKER_PIN);
 
 void timer1Task() {
+
   speaker.update();
 
   // 目標速度
   xyz_t target_vel{0, 0, 0};
   const float lost_time = 1000.0; // 通信が途切れてからロスト判定するまでの時間
-  if (millis() - receiver.updated_time() < lost_time) {
-      target_vel = receiver.vel();
+#ifndef USE_DABBLE
+  if (millis() - receiver.updated_time() < lost_time)
+  {
+    target_vel = receiver.vel();
+
+    Serial.printf("%f, %f, %f", receiver.vel().x, receiver.vel().y, receiver.vel().z);
+    Serial.println(receiver.kick_flag());
   }
+#endif
+
+#ifdef USE_DABBLE
+  // Dabble ゲームパッド
+  Dabble.processInput();
+  target_vel.x = GamePad.getYaxisData() * 50.0;
+  target_vel.y = GamePad.getXaxisData() * -50.0;
+  if (GamePad.isCirclePressed())
+  {
+    target_vel.z = -1;
+  }
+  if (GamePad.isSquarePressed())
+  {
+    target_vel.z = 1;
+  }
+#endif
 
   xyz_t out_vel = robo.execute(target_vel);
 
@@ -45,8 +80,6 @@ void timer1Task() {
   servo2.set_speed(( 0.000 * out_vel.x -1.000* out_vel.y + Robo::RADIUS * out_vel.z) / MAX_SPEED);
   servo3.set_speed((-0.866 * out_vel.x +0.500 * out_vel.y + Robo::RADIUS * out_vel.z) / MAX_SPEED);
 
-  Serial.printf("%f, %f, %f", receiver.vel().x, receiver.vel().y, receiver.vel().z);
-  Serial.println(receiver.kick_flag());
 }
 
 EspEasyTimer timer1(TIMER_GROUP_0, TIMER_0);
@@ -62,7 +95,9 @@ void setup() {
 
   Serial.begin(115200);
 
+#ifndef USE_DABBLE
   receiver.setup();
+#endif
 
   delay(1000);
 
@@ -77,6 +112,9 @@ void setup() {
   Speaker::tone_type doremi[]{{5, 20}, {4, 20}, {5, 20}, {0, 20}, {5, 20}, {4, 20}, {5, 20}, {0, 20}, {Speaker::STOP, 20}};
   speaker.set_melody(doremi);
 
+  // Dabble ゲームパッド
+  Dabble.begin("VSSL");
+
   // 加速度センサ用割り込み
   // interval 10ms
   timer1.begin(timer1Task, 10);
@@ -86,12 +124,19 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   // キック
+#ifndef USE_DABBLE
   if (receiver.kick_flag()) {
+#endif
+#ifdef USE_DABBLE
+  if (GamePad.isTrianglePressed()) {
+#endif
     servo0.set_angle(-60);
     delay(50);
     servo0.set_angle(-90);
     delay(100);
   }
   delay(10);
+
+
   
 }
