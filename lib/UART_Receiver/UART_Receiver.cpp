6e67862ba70924d186c56af52b7c95a1949e6dea
yotadori <yotakunhappy@gmail.com>
2024-08-17 09:38:55 +0900

    整数型で受信

--- lib/UART_Receiver/UART_Receiver.cpp
+++ lib/UART_Receiver/UART_Receiver.cpp
@@ -16,51 +16,26 @@ void UART_Receiver::setup() {
 
 void UART_Receiver::update()
 {
+    int val[4] = {0};
 
-    // 受信したデータを保存する変数
-    String receivedData;
-    float x, y, z;
-    int kick;
-
-    // データがあるか確認
-    if (Serial1.available())
+  //受信データがある場合if内を処理
+    if (Serial1.available() > 0)
     {
-        // 時刻を取得
         last_updated_time_ = millis();
-
-        receivedData = Serial1.readStringUntil('\n'); // データを1行（改行まで）読み取る
-
-        // カンマでデータを分割
-        int firstComma = receivedData.indexOf(',');
-        int secondComma = receivedData.indexOf(',', firstComma + 1);
-        int thirdComma = receivedData.indexOf(',', secondComma + 1);
-
-        if (firstComma > 0 && secondComma > firstComma && thirdComma > secondComma)
+        delay(10);
+        for (int i = 0; i < 4; i++)
         {
-            x = receivedData.substring(0, firstComma).toFloat();
-            y = receivedData.substring(firstComma + 1, secondComma).toFloat();
-            z = receivedData.substring(secondComma + 1, thirdComma).toFloat();
-            kick = receivedData.substring(thirdComma + 1).toInt();
-
-            // 受け取ったデータをシリアルモニタに表示
-            Serial.print("X: ");
-            Serial.println(x);
-            Serial.print("Y: ");
-            Serial.println(y);
-            Serial.print("Z: ");
-            Serial.println(z);
-            Serial.print("Kick: ");
-            Serial.println(kick);
-
-            kick_flag_ = (kick != 0);
-
-            vel_.x = x;
-            vel_.y = y;
-            vel_.z = z;
+            val[i] = Serial1.parseInt(); // 文字列データを数値に変換
         }
-        else
-        {
-            Serial.println("データ形式が不正です。");
+
+        while (Serial1.available() > 0)
+        { // 受信バッファクリア
+            char t = Serial1.read();
         }
+
+        vel_.x = val[0];
+        vel_.y = val[1];
+        vel_.z = val[2];
+        kick_flag_ = val[3] != 0;
     }
 }
