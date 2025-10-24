f9b385a981e0d7d8c8eb7231a8b2bb351e7674e7
yota <yotakunhappy@gmail.com>
2025-10-24 12:53:43 +0900

    use remotexy

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,8 @@
 #include "Speaker.h"
 #include "Gyro.h"
 #include "UltrasonicSensor.h"
-#include "Udp_Receiver.h"
+//#include "Udp_Receiver.h"
+#include "RemoteXY_Header.h"
 
 /*
 constexpr int SERVO_PIN = D7;
@@ -42,7 +43,7 @@ Robo robo = Robo(rot1, rot2, rot3, servo0, gyro);
 // スピーカー
 Speaker speaker = Speaker(4, SPEAKER_PIN);
 
-Udp_Receiver udp_receiver = Udp_Receiver("yota-HP-OmniBook", "yotakunhappy");
+// Udp_Receiver udp_receiver = Udp_Receiver("yota-HP-OmniBook", "yotakunhappy");
 
 // 割り込みの周期
 float cycle = 1;
@@ -89,17 +90,41 @@ void setup() {
   // 一度キック動作をはさんで、キッカーを引き戻す
   robo.kick();
 
-  udp_receiver.setup();
+  // udp_receiver.setup();
+
+  RemoteXY_Init();
 }  
 
-void loop() {
+int dribble_speed = 0;
 
-  udp_receiver.update();
-  robo.set_target_vel(udp_receiver.vel());
-  if (udp_receiver.kick_flag()) {
+void loop() {
+  RemoteXY_Handler();
+  robo.set_target_vel(RemoteXY.joystick_01_y * 2.0,
+                      RemoteXY.joystick_01_x * -2.0,
+                      RemoteXY.joystick_02_x / -30.0);
+  if (RemoteXY.button_01) {
     robo.kick();
   }
-  Serial1.printf("%c", '0' + udp_receiver.dribble_pow());
+
+  if (RemoteXY.button_02) {
+    dribble_speed++;
+    speaker.beep(5);
+    RemoteXY_delay(500);
+    speaker.stop();
+  } else if (RemoteXY.button_03) {
+    dribble_speed--;
+    speaker.beep(3);
+    RemoteXY_delay(500);
+    speaker.stop();
+  }
+  if (dribble_speed < 0) {
+    dribble_speed = 0;
+  } else if (dribble_speed > 9) {
+    dribble_speed = 9;
+  }
+
+  Serial1.printf("%c", '0' + dribble_speed);
+
 
   if (Serial.available() > 0) {
     String input = Serial.readString();
@@ -108,4 +133,6 @@ void loop() {
   if (Serial1.available() > 0) {
     Serial.printf("%s", Serial1.readString().c_str());
   }
+
+  RemoteXY_delay(10);
 }
